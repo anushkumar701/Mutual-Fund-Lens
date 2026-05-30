0f40bde517e0cbdcff32bc0d22a4993ab5854d53
FundLens <fundlens@deploy.com>
2026-05-30 19:11:21 +0530

    Remove Netlify redirects for Cloudflare Pages

--- build/client/theme-flash.js
+++
@@ -1,6 +0,0 @@
-(function() {
-  var theme = localStorage.getItem('fundlens_theme');
-  if (theme === 'dark' || (!theme && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    document.documentElement.classList.add('dark');
-  }
-})();
